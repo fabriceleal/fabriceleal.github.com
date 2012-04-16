bc698683fe354ad5ac1eeeef6ad50e325fb7a286
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-04-16 14:29:29 -0700

    tiny lib for ajax :|

---
+++ nice_ajax.js
@@ -0,0 +1,34 @@
+/*
+   Provide the XMLHttpRequest constructor for older versions of IE >:(
+ 
+	Based on:
+   http://blogs.msdn.com/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
+*/
+
+if (typeof XMLHttpRequest == "undefined")
+  XMLHttpRequest = function () {
+    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+      catch (e) {}
+    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+      catch (e) {}
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+      catch (e) {}
+    //Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
+    throw new Error("This browser does not support XMLHttpRequest.");
+};
+
+
+/*
+ Based on http://ajaxpatterns.org/XMLHttpRequest_Call#Aynschronous Calls
+*/
+function request_json(url, callback){
+	var req = new XMLHttpRequest();
+	req.open("GET", url, true);
+	req.onreadystatechanged = function(){
+		if(req.readyState != 4) { return; }
+		
+		var data = JSON.parse(req.responseText);
+		callback(data);
+	};
+	req.send(null);
+}
